2c26872560e6f2ddafaad5013247d5ae9e3be119
re2osushi8888 <re.2osushi8888@gmail.com>
2024-09-28 15:32:52 +0000

    フロントからAPIを叩けるようにした

--- frontend/app/hello_backend/page.tsx
+++ frontend/app/hello_backend/page.tsx
@@ -1,17 +1,19 @@
 'use client'
 
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Page() {
   const [data,setData] = useState({message: ''})
 
   useEffect(() => {
-    axios.get('http://host.docker.internal:7000/api/hello/backend/')
-      .then((res) => res.data)
-      .then((data) => {
-        setData(data)
-      })
+    // アクセスするのはホストのブラウザから。
+    // ホスト側のローカルホストにポートフォワードされたbackendに向かってAPIを投げる
+    axios.get('http://localhost:8000/api/hello/backend/')
+    .then((res) => res.data)
+    .then((data) => {
+      setData(data)
+    })
   })
 
   return <div>hello {data.message}!</div>
